13bf73ac51e15ab8afb020d32a1b8f444e642651
biosckon <biosckon@hotmail.com>
2016-10-15 08:06:25 +0200

    BROKEN need to fix...

--- 2.3.1_MergeSort/src/App.js
+++ 2.3.1_MergeSort/src/App.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 function _merge(A, p, q, r) {
-  let n1 = q - p + 1, n2 = r - 1;
+  let n1 = Math.round(q - p + 1), n2 = Math.round(r - 1);
   let L = Array(n1 + 1), R = Array(n2 + 1);
   for (let i = 0; i < n1; i += 1) { L[i] = A[p + i - 1]; }
   for (let j = 0; j < n2; j += 1) { R[j] = A[q + j]; }
+
   L[n1 + 1] = undefined; R[n2 + 1] = undefined;
   let i = 1, j = 1;
   for (let k = p; k < r; k++) {
     if (L[i] >= R[j]) { A[k] = L[i]; i += 1; }
     else { A[k] = R[j]; j += 1; }
   }
-  console.log(A);
 }
 
 function MergeSort(A, p, r) {
-    if (p < r) {
-      let q = (p + r)/2;
-      MergeSort(A, p, q);
-      MergeSort(A, q + 1, r);
-      _merge(A, p, q, r);
-    }
-    return A;
+  if (p < r) {
+    let q = Math.round((p + r)/2);
+    MergeSort(A, p, q);
+    MergeSort(A, q + 1, r);
+    _merge(A, p, q, r);
+  }
+  return A;
 }
 
 let a = [10, 3, 240, 34, 100, 2, 3, 5, 23];
 
 const IN = a.join(' ');
-const OUT = MergeSort(a).join(' ');
+const OUT = MergeSort(a, 0, a.length).join(' ');
 
 //####################################################
 //### Visualisation
